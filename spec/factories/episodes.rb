9213f8663fb855abc590f49f8ae29c0f340cc63f
Rubén Cabrera <vodka_esp@protonmail.com>
2024-03-25 21:56:50 +0100

    feat: added relationships to factories

--- spec/factories/episodes.rb
+++ spec/factories/episodes.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :episode, parent :content do
     title { Faker::Name.title }
     original_title { Faker::Name.title }
+    season { build(:season) }
   end
 end
--- spec/factories/seasons.rb
+++ spec/factories/seasons.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :season, parent :content do
     title { Faker::Name.title }
     original_title { Faker::Name.title }
+    tv_show { build(:tv_show) }
   end
 end
