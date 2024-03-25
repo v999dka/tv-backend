8e48e60159f112d805aee872ba5c1ca8f6d6c35a
Rubén Cabrera <vodka_esp@protonmail.com>
2024-03-25 20:23:44 +0100

    feat: added content

---
+++ db/migrate/20240325191842_add_content_model.rb
@@ -0,0 +1,14 @@
+class AddContentModel < ActiveRecord::Migration[7.1]
+  def change
+    create_table :content do
+      t.string :title
+      t.string :original_title
+      t.string :type
+      t.integer :number
+      t.integer :year
+      t.integer :parent_id
+
+      t.timestamps
+    end
+  end
+end
