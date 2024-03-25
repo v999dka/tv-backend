ba90697206688c3d83abfe17b6f0ae41445a5d9a
Rubén Cabrera <vodka_esp@protonmail.com>
2024-03-25 21:46:06 +0100

    feat: added relationship between TVShows and Seasons

--- app/models/season.rb
+++ app/models/season.rb
@@ -1,2 +1,4 @@
 class Season < Content
+  belongs_to :tv_show, foreign_key: 'parent_id'
+  has_many :seasons, foreign_key: 'parent_id', dependent: :destroy
 end
--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -1,2 +1,3 @@
 class TvShow < Content
+  has_many :seasons, foreign_key: 'parent_id', dependent: :destroy
 end
