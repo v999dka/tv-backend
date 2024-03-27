2841dd6a3bdebd3117e38d0efd88a9affb0fc6ee
Rubén Cabrera <vodka_esp@protonmail.com>
2024-03-27 18:46:29 +0100

    feat: added seeder and updated docker compose to wait on database correctly

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,23 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+list = List.find_or_create_by(name: 'default')
+(1..10).each do |i|
+  Movie.find_or_create_by(title: "Movie#{i}", original_title: "Original Movie#{i}", year: 2023)
+  TvShow.find_or_create_by(title: "TvShow#{i}", original_title: "Original TvShow#{i}", year: 2023)
+end
+
+TvShow.all.each_with_index do |show, index|
+  s = Season.find_or_create_by(title: "Season#{index}", original_title: "Season#{index}", year: 2023, number: index, tv_show: TvShow.find(show.id))
+
+  (1..5).each do |e|
+    Episode.find_or_create_by(title: "Episode#{e}", original_title: "Episode#{e}", year: 2023, number: e, season: Season.find(s.id))
+  end
+end
+
+[Movie.all, TvShow.all, Season.all, Episode.all].flatten.each do |content|
+  ListContent.find_or_create_by(list: list, content: content)
+end
+
+puts "Seeds created successfully"
